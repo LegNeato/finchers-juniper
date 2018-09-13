0f3917ef0d28aabba6b3a6c4ef9b2ae4c51abc1e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-09-13 18:29:22 +0000

    Deploy finchers-rs/finchers-juniper to github.com/finchers-rs/finchers-juniper.git:gh-pages

---
+++ implementors/finchers/endpoint/trait.Endpoint.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["finchers"] = [];
+implementors["finchers_juniper"] = [{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"finchers/endpoint/trait.Endpoint.html\" title=\"trait finchers::endpoint::Endpoint\">Endpoint</a>&lt;'a&gt; for <a class=\"struct\" href=\"finchers_juniper/struct.GraphiQL.html\" title=\"struct finchers_juniper::GraphiQL\">GraphiQL</a>",synthetic:false,types:["finchers_juniper::graphiql::GraphiQL"]},{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"finchers/endpoint/trait.Endpoint.html\" title=\"trait finchers::endpoint::Endpoint\">Endpoint</a>&lt;'a&gt; for <a class=\"struct\" href=\"finchers_juniper/struct.RequestEndpoint.html\" title=\"struct finchers_juniper::RequestEndpoint\">RequestEndpoint</a>",synthetic:false,types:["finchers_juniper::request::RequestEndpoint"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/finchers/output/trait.Output.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["finchers"] = [];
+implementors["finchers_juniper"] = [{text:"impl <a class=\"trait\" href=\"finchers/output/trait.Output.html\" title=\"trait finchers::output::Output\">Output</a> for <a class=\"struct\" href=\"finchers_juniper/struct.GraphQLResponse.html\" title=\"struct finchers_juniper::GraphQLResponse\">GraphQLResponse</a>",synthetic:false,types:["finchers_juniper::request::GraphQLResponse"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
